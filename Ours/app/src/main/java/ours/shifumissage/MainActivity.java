604c2cc8ea3c842903494d99cc0cb763a01ccfb1
AlexGreau <alexgreau@live.fr>
2018-05-28 00:39:34 +0200

    Permissions

--- Ours/app/src/main/java/ours/shifumissage/MainActivity.java
+++ Ours/app/src/main/java/ours/shifumissage/MainActivity.java
@@ -14,7 +14,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.View;
-import android.view.Menu;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
